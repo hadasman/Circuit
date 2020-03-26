127fa490a62b01ef1204bfebb6e7f5c632777523
Hadas Manor <hadasmanor@Hadass-MacBook-Air.local>
2020-03-26 13:46:16 +0200

    Fixed thalamic input after talking with Oren; Made pyramidal cell a Population.py object

--- Circuit.py
+++ Circuit.py
@@ -6,16 +6,21 @@ from neuron import gui, h
 import pdb, os, sys
 import matplotlib.pyplot as plt
 from Population import Population
+from math import log
 
 os.chdir('../MIT_spines')
 from Cell import Cell
 os.chdir('../Circuit')
 
-# ============================================  Define Functions & Constants  ============================================
-def analyzeConnectivityFromThalamus(which_layer, which_mtype):
+cell_details_filename		='thalamocortical_Oren/thalamic_data/cells_details.pkl'
+thalamic_locs_filename		='thalamocortical_Oren/thalamic_data/thalamic_axons_location.pkl'
+thal_connections_filename 	= 'thalamocortical_Oren/thalamic_data/thalamo_cortical_connectivity.pkl'
 
-	cell_details = pd.read_pickle('thalamocortical_Oren/Thalamo-cortical connections in the BBP/cells_details.pkl')
-	thal_connections = pd.read_pickle('thalamocortical_Oren/Thalamo-cortical connections in the BBP/Thalamo_cortical_connectivity.pkl')
+# ============================================  Define Functions & Constants  ============================================
+def analyzeConnectivityFromThalamus(which_layer, which_mtype, cell_details_filename=cell_details_filename, thal_connections_filename=thal_connections_filename):
+
+	cell_details = pd.read_pickle(cell_details_filename)
+	thal_connections = pd.read_pickle(thal_connections_filename)
 
 	if which_layer=='all':
 		which_layer = ''
@@ -48,7 +53,49 @@ def analyzeConnectivityFromThalamus(which_layer, which_mtype):
 
 	return stats
 
-def RunSim(PYR_cell, PV_pop, VIP_pop, v_init=-75, tstop=154*60):
+def getGIDs(pyr_type, PV_type):
+	
+	cell_details = pd.read_pickle(cell_details_filename)
+	pyr_gids, PV_gids = [], []
+
+	for i in range(len(cell_details)): 
+
+		if cell_details.iloc[i].mtype==pyr_type:
+			pyr_gids.append(cell_details.index[i])
+		elif cell_details.iloc[i].mtype==PV_type:
+			PV_gids.append(cell_details.index[i])
+
+	return pyr_gids, PV_gids
+
+def getPyramidalGID(pyr_gids, freq1, freq2, min_freq=4000, df_dx=3.5, cell_details_filename=cell_details_filename, thalamic_locs_filename=thalamic_locs_filename):
+	# df/dx is n units [octave/mm]
+
+	thalamic_locs = pd.read_pickle(thalamic_locs_filename)
+	cell_details = pd.read_pickle(cell_details_filename)
+	min_freq_loc = min(thalamic_locs.x) 
+
+	def get_AxonLoc(freq, min_freq, min_freq_loc, df_dx=df_dx):
+
+		dOctave = log(freq / min_freq, 2) # In octaves
+		d_mm =  dOctave / df_dx			  # In millimeters
+		freq_loc = min_freq_loc + d_mm
+
+		return freq_loc
+
+	freq1_loc = get_AxonLoc(freq1, min_freq, min_freq_loc)
+	freq2_loc = get_AxonLoc(freq2, min_freq, min_freq_loc)
+	mid_loc   = np.mean([freq1_loc, freq2_loc])
+
+	cur_min_dist = 100
+	for gid in pyr_gids:
+		if abs(mid_loc - cell_details.loc[gid].x) < cur_min_dist:
+			cur_min_dist = abs(mid_loc - cell_details.loc[gid].x)
+			cur_gid = gid
+
+	return cur_gid
+
+def RunSim(PYR_cell, PV_pop, VIP_pop, v_init=-75, tstop=170*60):
+	# Oren's simulation length is 154 seconds, I leave some time for last inputs to decay
 	t = h.Vector()
 	t.record(h._ref_t)
 	h.tstop = tstop
@@ -61,35 +108,42 @@ def RunSim(PYR_cell, PV_pop, VIP_pop, v_init=-75, tstop=154*60):
 	_, (ax1, ax2, ax3) = plt.subplots(3, 1)
 
 	ax1.plot(t, PYR_cell.soma_v)
-	ax1.set_title('Pyramidal cell voltage')
+	ax1.set_title('Pyramidal soma voltage')
 	ax1.set_xlabel('Time (ms)')
 	ax1.set_ylabel('Voltage (mV)')
 
+	for cell in PV_pop.cells:
+		ax2.plot(t, PV_pop.cells[cell]['soma_v'], label=cell)
+	ax2.set_title('PV soma coltage')
+	ax2.set_xlabel('Time (ms)')
+	ax2.set_ylabel('Voltage (mV)')
+
 	return PYR_cell, PV_pop, VIP_pop
 
-pyr_template_path 	= '../MIT_spines/cell_templates'
-pyr_template_name 	= 'whole_cell'
-pyr_morph_filename 	= 'cell1.asc',
-pyr_morph_path 		= 'L5PC/'
+pyr_template_path 	= 'EPFL_models/L23_PC_cADpyr229_1' # '../MIT_spines/cell_templates'
+pyr_template_name 	= 'cADpyr229_L23_PC_5ecbf9b163' # 'whole_cell'
+pyr_morph_filename 	= 'dend-C170897A-P3_axon-C260897C-P4_-_Clone_4.asc' # 'cell1.asc'
+pyr_morph_path 		= '{}/morphology'.format(pyr_template_path) # 'L5PC/'
 
 PV_template_path 	= 'EPFL_models/L23_LBC_cNAC187_1'
 PV_template_name 	= 'cNAC187_L23_LBC_df15689e81'
 PV_morph_filename 	= 'C050398B-I4_-_Clone_3.asc'
-PV_morph_path 		= 'EPFL_models/L23_LBC_cNAC187_1/morphology/'
+PV_morph_path 		= '{}/morphology/'.format(PV_template_path)
 
-thalamo_to_pyr_stats = analyzeConnectivityFromThalamus('L23', 'PC')
-thalamo_to_PV_stats = analyzeConnectivityFromThalamus('L23', 'LBC')
+# thalamo_to_pyr_stats = analyzeConnectivityFromThalamus('L23', 'PC')
+# thalamo_to_PV_stats  = analyzeConnectivityFromThalamus('L23', 'LBC')
 
-# Number of thalamic axons contacting each cell
-n_axons_to_pyr = thalamo_to_pyr_stats['per_cell']['mean_incoming_axons']
-n_axons_to_PV = thalamo_to_PV_stats['per_cell']['mean_incoming_axons']
+# # Number of thalamic axons contacting each cell
+# n_axons_to_pyr = thalamo_to_pyr_stats['per_cell']['mean_incoming_axons']
+# n_axons_to_PV  = thalamo_to_PV_stats['per_cell']['mean_incoming_axons']
 
-# Number of contacts a thalamic axon makes on each cell
-n_contacts_to_pyr = thalamo_to_pyr_stats['per_cell']['mean_incoming_contacts']
-n_contacts_to_PV = thalamo_to_PV_stats['per_cell']['mean_incoming_contacts']
+# # Number of contacts a thalamic axon makes on each cell
+# n_contacts_to_pyr = thalamo_to_pyr_stats['per_cell']['mean_incoming_contacts']
+# n_contacts_to_PV  = thalamo_to_PV_stats['per_cell']['mean_incoming_contacts']
 
-
-n_PV  		  = 1
+pyr_type = 'L4_PC'
+PV_type = 'L23_LBC'
+n_PV  		  = 4
 PV_density	  = 0.1 # Density of synapses from PV to pyramidal
 PV_dist 	  = 'one'
 
@@ -132,35 +186,48 @@ Pyramidal cell:
 	! Make sure my biophys is like Itay Hay's !
 '''
 
-print('Creating PV population')
+print('# Creating PV population')
 PV_pop = Population('PV', PV_morph_path, PV_morph_filename, PV_template_path, PV_template_name)
 for i in range(n_PV):
 	PV_pop.addCell()
-# PV_pop.addInput('thalamic', n_axons_to_PV, n_contacts_to_PV, frac_standard_axons = 0.5, filename='thalamocortical_Oren/SSA_spike_times/stim_times.p', which_standard=6666)
 
+print('# Creating pyramidal cell')
+Pyr_pop = Population('Pyr', pyr_morph_path, pyr_morph_filename, pyr_template_path, pyr_template_name)
+Pyr_pop.addCell()
 
-print('Creating pyramidal cell')
+'''
 PYR_cell = Cell(pyr_morph_path, 'pyr', pyr_morph_filename, pyr_template_name, pyr_template_path, verbose=False)
 PYR_cell.loadMorph()
 
 PYR_branches = [a for a in PYR_cell.cell.apic] + [d for d in PYR_cell.cell.dend]
 PYR_cell.soma_v = h.Vector()
 PYR_cell.soma_v.record(PYR_cell.cell.soma[0](0.5)._ref_v)
+'''
 
-# ==============================================  Connect Populations with Synapses  ==============================================
+# ==============================================  Connect Populations with Synapses and Inputs  ==============================================
+
+pyr_GIDs, PV_GIDs = getGIDs(pyr_type, PV_type)
+print('\nConnecting thalamic inputs to PV')
+for i, PV_cell_name in enumerate(PV_pop.cells):
+	PV_gid = PV_GIDs[i+10]
+	PV_pop.addInput(PV_cell_name, PV_gid, thalamic_activations_filename="thalamocortical_Oren/SSA_spike_times/input6666_106746.dat", thalamic_connections_filename=thal_connections_filename)
+
+print('Connecting thalamic inputs to Pyramidal cell')
+pyr_gid = getPyramidalGID(pyr_GIDs, 6000, 9600)
+Pyr_pop.addInput(list(Pyr_pop.cells.keys())[0], pyr_gid, thalamic_activations_filename="thalamocortical_Oren/SSA_spike_times/input6666_106746.dat", thalamic_connections_filename=thal_connections_filename)
 
 print('Connecting PV population to Pyramidal cell')
-
-# UNDER CONSTRUCTION
-# post_branches should be an array of real branch names (string) in PYR_cell, i.e. 'apic[1]' or 'dend[5]'
 PV_pop.connectCells([PV_pop.cells['PV0']['cell'].axon[1](1)]*3, [PYR_cell.cell.soma[0]], PV_density, PV_dist) # Adds self.connections to Population
-print('Assuming isopotential soma and perisomatic PV connections - all PV synapses are places on soma(0.5)')
-
-
+print('\n***Assuming isopotential soma and perisomatic PV connections: all PV synapses are placed on soma(0.5)')
 
 # ==============================================  Run Simulation & Plot Results  ==============================================
-
 PYR_cell, PV_pop, VIP_pop = RunSim(PYR_cell, PV_pop, VIP_pop)
+
+# ==============================================  Morphology Visualization  ==============================================
+
+
+for i in range(n_PV):  
+	PV_pop.moveCell(PV_pop.cells['PV{}'.format(i)]['cell'], i*300, -700, 0)  
 
 
 '''
--- Population.py
+++ Population.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os, pdb
 import _pickle as cPickle
+import pandas as pd
 os.chdir('../MIT_spines')
 from Cell import Cell
 from neuron import h, gui
@@ -24,6 +25,8 @@ class Population():
 		self.loadTemplate()
 		exec("self.template = h.%s"%template_name)
 
+
+
 	def loadTemplate(self):
 		"""
 		Load template file. This should be done only once for each template type! No need to reload
@@ -34,16 +37,17 @@ class Population():
 		"""
 
 		# Change directory to template folder
+
 		original_path = os.getcwd()
 		os.chdir(self.template_path)
 
 		h.load_file("stdrun.hoc")
-		print('Loading constants')		
+		print('- Loading constants')		
 		h.load_file('import3d.hoc')
 
 		constants_loaded = h.load_file('constants.hoc')
-		morphology_loaded = h.load_file('morphology.hoc')
-		biophysics_loaded = h.load_file('biophysics.hoc')
+		morphology_loaded = h.load_file('morphology_%s.hoc'%self.template_name)
+		biophysics_loaded = h.load_file('biophysics_%s.hoc'%self.template_name)
 
 		error = 'Can\'t find hoc file! Did you create it and call it by the correct name?'
 		if not constants_loaded:
@@ -57,7 +61,9 @@ class Population():
 		if self.verbose:
 			print('- Making %s template from .hoc file'%self.template_name)
 
-		h.load_file('%s.hoc'%self.template_name)
+		# h.load_file('%s.hoc'%self.template_name)
+		h.load_file('template_%s.hoc'%self.template_name)
+
 		# Return to original dir
 		os.chdir(original_path)
 
@@ -90,7 +96,253 @@ class Population():
 		# Return to original dir
 		os.chdir(original_path)
 
-	def addInput(self, cell, mode, n_axons, n_contacts_per_axon, frac_standard_axons = 0.5, filename=''):
+	def connectCells(self, pre_branches, post_branches, density, dist):
+		def addInhSynapses(branch, locs, presyn_seg, delay=0, weight=None, threshold=0, Dep=0, Fac=0):
+			'''locs and preysnaptics should be arrays ordered identically, for synapse location on postsynaptic branch (locs) and
+			presynaptic partner (presynaptics)
+			Inputs:
+				- branch: postsynaptic branch on which to place the synapses
+				- locs: location of each synapse on this branch.
+				- presyn_seg: segment on the presynaptic section (a place on the axon, i.e. axon[0](0.2)), 
+								from which the referenve to membrane voltage will be taken as input to branch. 
+								Ordered with respect to <locs> array.
+				- delay: synaptic delay between presynaptic threshold-crossing and postsynaptic activation. 
+				- weight: weight of the synapse (in units of nS).
+			'''
+
+			synapses, netcons = [], []
+			if not weight:
+				weight = 0.5 # Default value for inh. synapses
+
+			for i in range(len(locs)):
+				synapses.append(h.ProbUDFsyn2_lark(locs[i], sec = branch))
+				# pdb.set_trace()
+				netcons.append(h.NetCon(presyn_seg[i]._ref_v, synapses[-1], sec=presyn_seg[i].sec))
+
+				synapses[-1].tau_r 	= 0.18
+				synapses[-1].tau_d 	= 5
+				synapses[-1].e 		= -80
+				synapses[-1].Dep 	= Dep # Put depression on PV -> pyr synapses
+				synapses[-1].Fac 	= Fac
+				synapses[-1].Use 	= 0.25
+				synapses[-1].u0 	= 0
+				synapses[-1].gmax 	= 0.001 # don't touch - weight conversion factor to (us) (later multiplied by the conductance in nS)
+
+				netcons[-1].weight[0] = weight # In units nS
+				netcons[-1].delay 	 = delay
+				netcons[-1].threshold = threshold
+
+			return synapses, netcons
+
+		def synLocations(L, density, dist):
+			"""
+			Returns synapse location according to given distribution, in arbitrary units (fraction [0-1]).
+			Arbitrary units (relative to branch length) is the way NEURON receives synapse locations.
+			"""
+			ok = 0
+			n_syns = int(np.ceil(density * L))	
+
+			while not ok:
+				if dist == 'uniform':
+					ok = 1
+
+					locs = sorted(np.arange(0, L, 1/density))
+					locs = [i/L for i in locs]
+
+					if len(locs) != n_syns:
+						print('Sanity check warning: unexpected locs length!')
+						pdb.set_trace()
+
+				elif dist == 'random':
+					ok = 1
+									
+					locs = np.random.rand(n_syns)
+					locs = sorted(pi/L for i in locs)
+				
+				elif dist == 'one':
+					ok = 1
+
+					locs = [0.5] * n_syns
+
+				else:
+					dist = input('Which synapse distribution for %s population? (uniform/random/one) '%self.population_name)
+
+			return locs
+
+		# _ = messagebox.showinfo(message='Write function for synapse placements!')
+
+		post_names = [i.name() for i in post_branches]
+		self.connections = {post_branch: {'locs': [], 
+										 'stim_axon_segs': [], 
+										 'synapses': [], 
+										 'netcons': []}  for post_branch in post_names}
+
+		# Assuming locs and presynaptic stim_axons have been determined and put into self.connections dict
+		for branch in self.connections:
+			branch_obj = [i for i in post_branches if branch in i.name()][0]
+			self.connections[branch]['locs'] 			= synLocations(branch_obj.L, density, dist)
+			self.connections[branch]['stim_axon_segs'] 	= pre_branches
+			# Think about weight, delay (synaptic), threshold and noise (stochastic firing)
+			
+			pdb.set_trace()
+			self.connections[branch]['synapses'], self.connections[branch]['netcons'] = addInhSynapses(
+																			branch_obj, 
+																			self.connections[branch]['locs'], 
+																			self.connections[branch]['stim_axon_segs'], 
+																			delay=0, weight=None, threshold=0, Dep=0)
+
+	def moveCell(self, cell, dx, dy, dz):
+		'''
+		Moves cell morphology.
+		When changing 3d coordinates of reconstructed cells, only change the soma location! 
+		The rest will follow because it's connected: when a section is moved all of its children apparently move too.
+		'''
+
+		soma = cell.soma[0]
+		for i in range(soma.n3d()):
+			h.pt3dchange(i, soma.x3d(i)+dx, soma.y3d(i)+dy, soma.z3d(i)+dz, soma.diam, sec=soma)
+
+	def addInput(self, cell_name, cell_gid, thalamic_activations_filename = '', thalamic_connections_filename=''):
+
+		def checkValidFilename(filename, cond='', which_standard=None):	
+			ok = 0	
+			original_path = os.getcwd()
+
+			path = filename.rpartition('/')[0]
+			file = filename.rpartition('/')[-1]
+			os.chdir(path)
+			
+			while not ok:
+
+				# This bloc is inside the while loop because condition should change according to new filename
+				if cond == 'exist':
+					condition = 'not \'%s\''%filename		
+				elif cond == 'in_dir':
+					condition = '\'%s\' not in os.listdir()'%file
+				else:
+					print('Filename validity check not provided with condition, exiting...')
+					return filename
+
+				if eval(condition):
+					filename = input('Oops, file not %s!\nEnter valid thalamic input file name with ext. (current dir: %s): '%(cond, os.getcwd()))
+				else:
+					ok = 1
+
+			os.chdir(original_path)
+			return filename
+
+		def processData(filename):
+
+			temp_data = [i.strip().split() for i in open(filename).readlines()]
+			data = []
+			for i in range(len(temp_data)):
+				if temp_data[i][0].replace('.', '').isdigit():				
+					data.append([float(temp_data[i][0]), int(float(temp_data[i][1]))])
+
+			return data
+		
+		def getInputs(cell_gid, thalamic_activations, thalamic_connections_filename):
+			thal_connections = pd.read_pickle(thalamic_connections_filename)
+			connecting_gids = []
+			
+			# Find thalamic GIDs connecting the the pyramidal cell
+			for con in thal_connections.iterrows():
+				if con[1].post_gid == cell_gid:
+					connecting_gids.append([con[1].pre_gid, con[1].contacts]) # [presynaptic gid, no. of contacts]
+
+			# Get tha thalamic activation timings and no. of contacts on the pyramidal cell
+			cell_inputs = {i[0]: {'contacts': i[1], 'times': []} for i in connecting_gids}
+			for i in thalamic_activations:
+				if i[1] in cell_inputs.keys():
+					cell_inputs[i[1]]['times'].append(i[0])
+
+			return cell_inputs
+
+		def synLocationsAll(branches, n_syns):
+
+			branch_locs = {}
+			n_branches = len(branches)
+				
+			# Select branch and location at random
+			for i in range(n_syns):
+				rand_branch_idx  = np.random.randint(n_branches)
+				rand_branch 	 = branches[rand_branch_idx]
+				rand_branch_name = rand_branch.name().split('].')[-1]
+				rand_loc 		 = np.random.rand()
+				
+				if rand_branch_name in branch_locs.keys():
+					branch_locs[rand_branch_name]['locs'].append(rand_loc)
+				else:
+					branch_locs[rand_branch_name] 				= {}
+					branch_locs[rand_branch_name]['locs'] 	    = [rand_loc]
+					branch_locs[rand_branch_name]['branch_obj'] = rand_branch
+
+			return branch_locs
+
+		def addExcSynapses(contact_locs_dict, stim_times, delay=0, weight=None, threshold=0, Dep=0, Fac=0):
+			# contact_locs_dict = {branch_name: {'locs': [locs], 'branch_obj': branch_obj}}
+
+			print('* \nThese synapses are without depression. Find synapses with! *')
+
+			synapses, netcons, events = [], [], []
+
+			# Create synapse
+			for branch_name in contact_locs_dict: 
+				branch = contact_locs_dict[branch_name]['branch_obj']
+				
+				for loc in contact_locs_dict[branch_name]['locs']: 
+					synapses.append(h.ProbAMPANMDA2_RATIO(loc, sec=branch))
+					synapses[-1].Dep = Dep
+					synapses[-1].Fac = Fac
+
+			# NetCon: "The source may be a NULLObject. In this case events can only occur by calling event() from hoc"
+			for syn in synapses:
+				netcons.append(h.NetCon(None, syn))
+				netcons[-1].weight[0] = weight
+
+				# Custom initialization: insert event to queue (if h.FInitializeHandler no called, event is erased by h.run() because it clears the queue)
+				for time in stim_times:
+					events.append(h.FInitializeHandler('nrnpython("netcons[-1].event({})")'.format(time+delay)))
+
+			return synapses, netcons, events
+
+		# Check filenames are valid
+		thalamic_connections_filename = checkValidFilename(thalamic_connections_filename, cond='exist')
+		thalamic_connections_filename = checkValidFilename(thalamic_connections_filename, cond='in_dir')
+
+		thalamic_activations_filename = checkValidFilename(thalamic_activations_filename, cond='exist')
+		thalamic_activations_filename = checkValidFilename(thalamic_activations_filename, cond='in_dir')
+
+		# Open thalamic activations file and process data
+		thalamic_activations = processData(thalamic_activations_filename)
+
+		# For the given cell, find: presynaptic thalamic GIDs, no. of contacts each one makes and their activation timings
+		print('Finding presynaptic contacts for cell {}'.format(cell_name))
+		cell_inputs = getInputs(cell_gid, thalamic_activations, thalamic_connections_filename)
+		
+		# Add thalamic activation as synapses to cell
+		cell_obj 	 = self.cells[cell_name]['cell']
+		branches 	 = self.cells[cell_name]['dendrites']
+
+		# CHECK THIS!
+		print('Adding thalamo-cortical synapses and creating activation events')		
+		for thal_gid in cell_inputs:
+			n_syns 		= cell_inputs[thal_gid]['contacts']
+			stim_times 	= cell_inputs[thal_gid]['times']
+
+			contact_locs_dict = synLocationsAll(branches, n_syns)
+
+			print('\nNow all contacts are in one synapses! Check if contacts should affect weight or be different contacts in different locations')
+
+			temp_synapses, temp_netcons, _ = addExcSynapses(contact_locs_dict, stim_times, delay=0, 
+																							weight=0.4, 
+																							threshold=0, 
+																							Dep=0, Fac=0)				
+			self.inputs[cell_name]['thalamic_gid_{}'.format(thal_gid)]['synapses']  = temp_synapses
+			self.inputs[cell_name]['thalamic_gid_{}'.format(thal_gid)]['netcons']   = temp_netcons
+
+
+	def OLD_addInput(self, cell, mode, n_axons, n_contacts_per_axon, frac_standard_axons = 0.5, filename=''):
 		"""
 		Add presynaptic synapses. The mode variable determines where the synaptic information is taken from.
 
@@ -250,109 +502,3 @@ class Population():
 					\n* Make each thalamic gid correspond to a single netstim- connect all synapsesfrom gid to this netstim *')
 				_ = messagebox.showinfo(message='Thalamic input under construction!')		
 	
-	def connectCells(self, pre_branches, post_branches, density, dist):
-		def addInhSynapses(branch, locs, presyn_seg, delay=0, weight=None, threshold=0, Dep=0, Fac=0):
-			'''locs and preysnaptics should be arrays ordered identically, for synapse location on postsynaptic branch (locs) and
-			presynaptic partner (presynaptics)
-			Inputs:
-				- branch: postsynaptic branch on which to place the synapses
-				- locs: location of each synapse on this branch.
-				- presyn_seg: segment on the presynaptic section (a place on the axon, i.e. axon[0](0.2)), 
-								from which the referenve to membrane voltage will be taken as input to branch. 
-								Ordered with respect to <locs> array.
-				- delay: synaptic delay between presynaptic threshold-crossing and postsynaptic activation. 
-				- weight: weight of the synapse (in units of nS).
-			'''
-
-			synapses, netcons = [], []
-			if not weight:
-				weight = 0.5 # Default value for inh. synapses
-
-			for i in range(len(locs)):
-				synapses.append(h.ProbUDFsyn2_lark(locs[i], sec = branch))
-				# pdb.set_trace()
-				netcons.append(h.NetCon(presyn_seg[i]._ref_v, synapses[-1], sec=presyn_seg[i].sec))
-
-				synapses[-1].tau_r 	= 0.18
-				synapses[-1].tau_d 	= 5
-				synapses[-1].e 		= -80
-				synapses[-1].Dep 	= Dep # Put depression on PV -> pyr synapses
-				synapses[-1].Fac 	= Fac
-				synapses[-1].Use 	= 0.25
-				synapses[-1].u0 	= 0
-				synapses[-1].gmax 	= 0.001 # don't touch - weight conversion factor to (us) (later multiplied by the conductance in nS)
-
-				netcons[-1].weight[0] = weight # In units nS
-				netcons[-1].delay 	 = delay
-				netcons[-1].threshold = threshold
-
-			return synapses, netcons
-
-		def synLocations(L, density, dist):
-			"""
-			Returns synapse location according to given distribution, in arbitrary units (fraction [0-1]).
-			Arbitrary units (relative to branch length) is the way NEURON receives synapse locations.
-			"""
-			ok = 0
-			n_syns = int(np.ceil(density * L))	
-
-			while not ok:
-				if dist == 'uniform':
-					ok = 1
-
-					locs = sorted(np.arange(0, L, 1/density))
-					locs = [i/L for i in locs]
-
-					if len(locs) != n_syns:
-						print('Sanity check warning: unexpected locs length!')
-						pdb.set_trace()
-
-				elif dist == 'random':
-					ok = 1
-									
-					locs = np.random.rand(n_syns)
-					locs = sorted(pi/L for i in locs)
-				
-				elif dist == 'one':
-					ok = 1
-
-					locs = [0.5] * n_syns
-
-				else:
-					dist = input('Which synapse distribution for %s population? (uniform/random/one) '%self.population_name)
-
-			return locs
-
-		# _ = messagebox.showinfo(message='Write function for synapse placements!')
-
-		post_names = [i.name() for i in post_branches]
-		self.connections = {post_branch: {'locs': [], 
-										 'stim_axon_segs': [], 
-										 'synapses': [], 
-										 'netcons': []}  for post_branch in post_names}
-
-		# Assuming locs and presynaptic stim_axons have been determined and put into self.connections dict
-		for branch in self.connections:
-			branch_obj = [i for i in post_branches if branch in i.name()][0]
-			self.connections[branch]['locs'] 			= synLocations(branch_obj.L, density, dist)
-			self.connections[branch]['stim_axon_segs'] 	= pre_branches
-			# Think about weight, delay (synaptic), threshold and noise (stochastic firing)
-			
-			pdb.set_trace()
-			self.connections[branch]['synapses'], self.connections[branch]['netcons'] = addInhSynapses(
-																			branch_obj, 
-																			self.connections[branch]['locs'], 
-																			self.connections[branch]['stim_axon_segs'], 
-																			delay=0, weight=None, threshold=0, Dep=0)
-
-	def moveCell(self, cell, dx, dy, dz):
-		'''
-		Moves cell morphology.
-		When changing 3d coordinates of reconstructed cells, only change the soma location! 
-		The rest will follow because it's connected: when a section is moved all of its children apparently move too.
-		'''
-
-		soma = cell.soma[0]
-		for i in range(soma.n3d()):
-			h.pt3dchange(i, soma.x3d(i)+dx, soma.y3d(i)+dy, soma.z3d(i)+dz, soma.diam, sec=soma)
-
